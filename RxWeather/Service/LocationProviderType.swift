4c72192863b42cfb2871f259ae211728ead7f695
DustinPark92 <p4569zz@gmail.com>
2021-02-17 14:01:22 +0900

    static location

--- RxWeather/Service/LocationProviderType.swift
+++ RxWeather/Service/LocationProviderType.swift
@@ -23,3 +23,15 @@
 import Foundation
 import CoreLocation
 import RxSwift
+
+
+
+protocol LocationProviderType {
+    @discardableResult
+    func currentLocation() -> Observable<CLLocation>
+    
+    @discardableResult
+    func currentAddress() -> Observable<String>
+    
+    
+}
